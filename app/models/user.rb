c6edd5611ce9ccf59a7242cab41fbe7506d71f0a
jeromeall <jeromeallouche01@gmail.com>
2014-03-09 17:26:51 -0700

    added model relationship to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+has_many :transitions
+has_many :spots, through: :transitions
+
+    #validates :first_name, presence: true, length: { maximum: 50 }
+    #validates :last_name, presence: true, length: { maximum: 50 }         
 end
