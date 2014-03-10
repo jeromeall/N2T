e7823334da48dbe34630407928b7ae3f4a6f5883
jeromeall <jeromeallouche01@gmail.com>
2014-03-09 17:09:16 -0700

    added default url for development and production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ NewToTown::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'http://nameless-bayou-6479.herokuapp.com/' }
 end
