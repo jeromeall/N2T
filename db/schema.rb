034ae38700f94861bddf74acffe9a3dd1b32cf2a
jeromeall <jeromeallouche01@gmail.com>
2014-03-09 18:28:27 -0700

    replaced first&last name with name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140310002303) do
+ActiveRecord::Schema.define(version: 20140310012748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,13 +58,12 @@ ActiveRecord::Schema.define(version: 20140310002303) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "first_name"
-    t.string   "last_name"
     t.string   "city"
     t.integer  "state_code"
     t.integer  "zip"
     t.string   "neighborhood"
     t.text     "about"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
