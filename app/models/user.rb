12ae6a1b72f9ebb1d419a09db49470d69c6f1049
jeromeall <jeromeallouche01@gmail.com>
2014-03-11 18:25:44 -0700

    fixed indentations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,12 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
 
-has_many :transitions
-has_many :spots, through: :transitions
+  has_many :transitions
+  has_many :spots, through: :transitions
 
-    #validates :first_name, presence: true, length: { maximum: 50 }
-    #validates :last_name, presence: true, length: { maximum: 50 }         
+  #validates :first_name, presence: true, length: { maximum: 50 }
+  #validates :last_name, presence: true, length: { maximum: 50 }         
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,20 @@
 NewToTown::Application.routes.draw do
 
-devise_for :users, :skip => [:sessions]
-as :user do
-  get 'sign-in' => 'devise/sessions#new', :as => :new_user_session
-  post 'sign-in' => 'devise/sessions#create', :as => :user_session
-  delete 'sign-out' => 'devise/sessions#destroy', :as => :destroy_user_session
-  get 'users/:id' => 'devise/registrations#show', :as => :user
-end
+  devise_for :users, :skip => [:sessions]
+  as :user do
+    get 'sign-in' => 'devise/sessions#new', :as => :new_user_session
+    post 'sign-in' => 'devise/sessions#create', :as => :user_session
+    delete 'sign-out' => 'devise/sessions#destroy', :as => :destroy_user_session
+    get 'users/:id' => 'devise/registrations#show', :as => :user
+  end
 
 
-root to: "site#index"
+  root to: "site#index"
 
-resources :spots
-get "/spots/:id", to: "spots#view_user_spot"
-
-
-
-
-post "/:yelp_id", to: "spots#add_to_user"
-get "/:yelp_id", to: "spots#result", as: "business"
-post "/spots/:id", to: "spots#update"
+  resources :spots
+  get "/spots/:id", to: "spots#view_user_spot"
+  post "/:yelp_id", to: "spots#add_to_user"
+  get "/:yelp_id", to: "spots#result", as: "business"
+  post "/spots/:id", to: "spots#update"
 
 end
